27c0fcaf8f0f00f09395a3e8bf3049c3910de287
gauravtrivedi967 <gauravtrivedi967>
2023-08-04 20:52:02 +0530

    updated nodejs scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,46 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
-from functools import lru_cache
-
-@lru_cache(maxsize=128)  # Cache up to 128 most recently used requests
-def fetch_page_content(keyword):
-    base_url = "https://nodejs.org/docs/latest-v20.x/api/"
-    search_url = f"{base_url}{keyword}.html"
-
-    response = requests.get(search_url)
-    if response.status_code != 200:
-        return None
-
-    return response.content
 
 def scrape_nodejs_docs(keyword):
-    content = fetch_page_content(keyword)
-    if not content:
-        print(f"Could not retrieve data for '{keyword}'")
-        return []
+    base_url = "https://nodejs.org/docs/latest-v20.x/api/"
+    url = base_url + keyword + ".html"
 
-    soup = BeautifulSoup(content, "html.parser")
+    response = requests.get(url)
 
-    # Extract the function names and descriptions
-    function_details = []
-    h2_tags = soup.find_all("h2")
-    for h2 in h2_tags:
-        function_name = h2.text.strip()
-        next_element = h2.find_next_sibling()
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        section = soup.find('section', {'class': 'sect1'})
 
-        if next_element and next_element.name == "p":
-            description = next_element.text.strip()
-            function_details.append((function_name, description))
+        if section:
+            title = section.find('h1').text.strip()
+            description = section.find('p').text.strip()
 
-    return function_details
+            print(f"Title: {title}")
+            print(f"Description: {description}")
+        else:
+            print(f"Keyword '{keyword}' not found in Node.js documentation.")
+    else:
+        print(f"Failed to fetch the documentation for '{keyword}'. Check if the keyword is valid.")
 
 if __name__ == "__main__":
-    keyword = input("Enter the keyword to search in Node.js documentation: ")
-    result = scrape_nodejs_docs(keyword)
-    if result:
-        print(f"Found {len(result)} functions for '{keyword}':")
-        for i, (function_name, description) in enumerate(result, 1):
-            print(f"{i}. {function_name}")
-            print(f"   {description}")
-    else:
-        print(f"No functions found for '{keyword}'")
+    keyword = input("Enter a keyword to search in Node.js documentation: ")
+    scrape_nodejs_docs(keyword)
