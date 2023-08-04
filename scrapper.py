048daf1f53e7fe658bb97e4138caaff8564f79ec
gauravtrivedi967 <gauravtrivedi967>
2023-08-04 19:49:16 +0530

    updated scrapping code

--- scrapper.py
+++ scrapper.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from functools import lru_cache
 
-@lru_cache(maxsize=128)  
+@lru_cache(maxsize=128)  # Cache up to 128 most recently used requests
 def fetch_page_content(keyword):
-    base_url = "https://nodejs.org/dist/latest-v16.x/docs/api/"
+    base_url = "https://nodejs.org/docs/latest-v20.x/api/"
     search_url = f"{base_url}{keyword}.html"
 
     response = requests.get(search_url)
@@ -21,17 +21,26 @@ def scrape_nodejs_docs(keyword):
 
     soup = BeautifulSoup(content, "html.parser")
 
-    
-    function_names = [h2.text for h2 in soup.find_all("h2")]
+    # Extract the function names and descriptions
+    function_details = []
+    h2_tags = soup.find_all("h2")
+    for h2 in h2_tags:
+        function_name = h2.text.strip()
+        next_element = h2.find_next_sibling()
 
-    
-    return function_names
+        if next_element and next_element.name == "p":
+            description = next_element.text.strip()
+            function_details.append((function_name, description))
+
+    return function_details
 
 if __name__ == "__main__":
     keyword = input("Enter the keyword to search in Node.js documentation: ")
     result = scrape_nodejs_docs(keyword)
     if result:
         print(f"Found {len(result)} functions for '{keyword}':")
-        print("\n".join(result))
+        for i, (function_name, description) in enumerate(result, 1):
+            print(f"{i}. {function_name}")
+            print(f"   {description}")
     else:
         print(f"No functions found for '{keyword}'")
