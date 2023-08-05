66c0536d5abc90358c8811fbb4ddbd03084881b4
gauravtrivedi967 <gauravtrivedi967>
2023-08-05 22:12:13 +0530

    Updated frontend and flask

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
-from Nodejs_scrapper import scrape_nodejs_docs
+from Nodejs_scrapper import scrape_nodejs_documentation
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        keyword = request.form["keyword"]
-        result = scrape_nodejs_docs(keyword)
-        return render_template("index.html", result=result, keyword=keyword)
-    return render_template("index.html")
+        keyword = request.form.get("keyword")
+        if keyword:
+            description = scrape_nodejs_documentation(keyword)
+            return render_template("index.html", description=description)
+    
+    return render_template("index.html", description=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
